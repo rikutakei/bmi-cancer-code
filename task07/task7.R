11238ef36660e75bbea7adf02f6596983fbde7a1
rikutakei <rikut777@gmail.com>
2016-01-13 10:39:11 +1300

    finished making data using tidyr

--- task07/task7.R
+++ task07/task7.R
@@ -39,12 +39,23 @@ for(i in 1:length(files)) {
      assign(cancertypes[i],fread(files[i]))
 }
 
+#Convert the TCGA data into matrix form using tidyr:spread function
+
 for(i in 1:length(cancertypes)) {
     t = get(cancertypes[i])
-    t = t[,c('submitted_sample_id','gene_id','raw_read_count')]
-    dup = duplicated(paste(t$submitted_sample_id,t$gene_id,sep=''))
-    t = t[!dup,]
-    t = spread(t,submitted_sample_id,raw_read_count)
+    t = t[,c('submitted_sample_id','gene_id','raw_read_count'),with=F] # pull out the required information
+    dup = duplicated(paste(t$submitted_sample_id,t$gene_id,sep='')) #find duplicated genes
+    t = t[!dup,] # remove duplicated genes
+    t = spread(t,submitted_sample_id,raw_read_count) # make matrix
+    rownames(t) = t[,gene_id] # change rownames into gene names
+    t = data.matrix(t) #change it into matrix from data frame/table
+    t = t[-1,-1] # remove the gene column and ? row
     txt = paste(cancertypes[i],'mat',sep='')
     assign(txt,t)
+}
+
+for(i in 1:length(cancertypes)) {
+    txt = paste(cancertypes[i],'mat',sep='')
+    t = get(txt)
+    dput(t,file=paste(txt,'.txt',sep=''))
 }
