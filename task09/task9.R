a3848256414d4af9da89c0dfb72b59e0c066c3c2
rikutakei <rikut777@gmail.com>
2016-01-25 13:35:00 +1300

    starting task 9

---
+++ task09/task9.R
@@ -0,0 +1,24 @@
+######################################################################
+
+                        ##R code for task9
+
+######################################################################
+
+## Start analysis of other cancer data produced from tidyr
+
+######################################################################
+## pre-analysis stuff
+setwd('~/Documents/masters/data/task09/')
+
+library(data.table)
+library(limma)
+library(edgeR)
+library(DESeq)
+library(dplyr)
+library(gplots)
+library(affy)
+library(WGCNA)
+library(hgu133a.db)
+library(tidyr)
+######################################################################
+
